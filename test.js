e7519b1ea14b89f253a08a772e5649710f37acdb
Terkel Gjervig <terkel@terkel.com>
2019-11-03 14:51:43 -0500

    remove test file

--- test.js
+++
@@ -1,8 +0,0 @@
-const hent = require(`./dist/hent`);
-const fs = require(`fs`).promises;
-
-(async function () {
-	const url = `https://raw.githubusercontent.com/terkelg/hent/master/logo.png`;
-	const res = await hent(url);
-	await fs.writeFile('logo.png', res.buffer);
-})();
